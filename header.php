0a2b47489e0f41029590c218b813eaf73175caad
fkazu <kazukifuji@users.noreply.github.com>
2020-09-12 08:40:32 +0000

    Display post list
    投稿リストを表示。

--- header.php
+++ header.php
@@ -3,10 +3,13 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head(); ?>
+    <?php wp_head(); ?>
   </head>
   <body <?php body_class( 'body' ); ?>>
+    <?php wp_body_open(); ?>
 
     <div class="content">
     
-      <?php if ( is_home() || is_fromt_page() ) get_template_part( 'template_parts/hero-header' ); ?>
+      <?php if ( is_home() || is_front_page() ) get_template_part( 'template_parts/hero-header' ); ?>
+
+      <div class="main">
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php get_header(); ?>
 
+  <article>
+  
+    <?php get_template_part('template_parts/post-list'); ?>
+
+  </article>
+
 <?php get_footer(); ?>
