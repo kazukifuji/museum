63b44914b8414734e0dc857cd722232f15fb8cfd
fkazu <kazukifuji@users.noreply.github.com>
2020-09-17 08:16:10 +0000

    Add a feature to add posts to the post list based on the 'load_count' URL parameter
    'load_count'URLパラメータを元に、投稿リストに投稿を追加する機能を作成。

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
-locate_template('./functions/init.php', true);    //テーマの初期設定
-locate_template('./functions/nav-menu.php', true);    //ナビゲーションメニューの設定
-locate_template('./functions/widgets.php', true); //ウィジェットの設定
-locate_template('./functions/scripts.php', true); //外部css, jsファイルの読み込み
+locate_template('./functions/init.php', true);      //テーマの初期設定
+locate_template('./functions/content.php', true);   //コンテンツ部分の関数など
+locate_template('./functions/nav-menu.php', true);  //ナビゲーションメニューの設定
+locate_template('./functions/widgets.php', true);   //ウィジェットの設定
+locate_template('./functions/scripts.php', true);   //外部css, jsファイルの読み込み
