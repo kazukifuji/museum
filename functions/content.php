95f25e2cb7105133c496cc9486e9ab21d5a03a32
fkazu <kazukifuji@users.noreply.github.com>
2020-09-29 07:30:10 +0000

    Add HTML markup heading element to main block
    mainブロックにHTMLマークアップされたheadingエレメントを追加。

--- functions/content.php
+++ functions/content.php
@@ -13,33 +13,10 @@ add_filter( 'get_the_archive_title', function($title) {
 
   //カテゴリー、タグアーカイブ
   } elseif ( is_category() || is_tag() || is_tax() ) {
-
-    if ( is_category() || is_tax() ) {
-      $icon = <<<EOD
-      <svg class="post-list__heading-icon-svg" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
-        <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-      </svg>
-      EOD;
-
-    } else if ( is_tag() ) {
-      $icon = <<<EOD
-      <svg class="post-list__heading-icon-svg" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" >
-        <path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"></path>
-        <line x1="7" y1="7" x2="7.01" y2="7"></line>
-      </svg>
-      EOD;
-    }
-
     $title = single_cat_title('', false);
     
   //日時アーカイブ
   } else if ( is_date() ) {
-    $icon = <<<EOD
-    <svg class="post-list__heading-icon-svg" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
-      <circle cx="12" cy="12" r="10"></circle>
-      <polyline points="12 6 12 12 16 14"></polyline>
-    </svg>
-    EOD;
 
     //年別
     if ( is_year() ) {
@@ -54,25 +31,12 @@ add_filter( 'get_the_archive_title', function($title) {
 
   //投稿著者アーカイブ
   } else if ( is_author() ) {
-    $icon = <<<EOD
-    <svg class="post-list__heading-icon-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"">
-      <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle>
-    </svg>
-    EOD;
     $title = get_queried_object()->data->display_name;
 
   //検索結果ページ
   } else if ( is_search() ) {
-    $icon = <<<EOD
-    <svg class="post-list__heading-icon-svg" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
-      <circle cx="11" cy="11" r="8"></circle>
-      <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-    </svg>
-    EOD;
     $title = '「' . esc_html( get_search_query() ) . '」の検索結果';
   }
-
-  if ( isset( $icon ) ) echo '<span class="post-list__heading-icon">' . $icon . '</span>';
 
   return $title;
 } );
