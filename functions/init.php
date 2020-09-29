e26560d1b574228ed0fc55b635041fb0e2e8d67b
fkazu <kazukifuji@users.noreply.github.com>
2020-09-29 11:56:47 +0000

    Add WordPress theme support of 'custom-header' and register default header image
    'custom-header'のWordPressテーマサポートを追加し、デフォルトのヘッダー画像を登録。

--- functions/init.php
+++ functions/init.php
@@ -7,6 +7,26 @@ add_action( 'after_setup_theme', function() {
   add_theme_support( 'title-tag' );
   add_theme_support( 'post-thumbnails' );
   add_theme_support( 'automatic-feed-links' );
+  add_theme_support( 'custom-header', [
+    'default-image' => esc_url( get_template_directory_uri() . '/assets/images/header.jpg' ),
+    'width'              => '1200',
+    'height'             => '800',
+    'flex-width'         => true,
+    'flex-height'        => true,
+    'header-text'        => false,
+    'default-text-color' => 'FFF',
+    'video'              => false,
+    'uploads'            => false,
+    'random-default'     => false,
+  ] );
+
+  //デフォルトのヘッダー画像を登録
+  register_default_headers([
+    'default-image' => [
+      'url' => esc_url( get_template_directory_uri() . '/assets/images/header.jpg' ),
+      'thumbnail_url' => esc_url( get_template_directory_uri() . '/assets/images/header.jpg' ),
+    ],
+  ]);
 } );
 
 //$content_widthを設定
