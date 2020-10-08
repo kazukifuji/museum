b53ee905d05d75b7eb7758d3a1a5d3708d1ac3e1
fkazu <kazukifuji@users.noreply.github.com>
2020-10-08 04:47:50 +0000

    Remove anonymous function from comments.php
    comments.phpから無名関数を削除。

--- functions/comments.php
+++ functions/comments.php
@@ -1,57 +1,49 @@
 <?php
-//コメント欄の設定
+//コメントのオートリンク機能を無効化
+remove_filter( 'comment_text', 'make_clickable', 9 );
 
-//コメントフォームをカスタマイズ
-add_filter( 'comment_form_defaults', function($args) {
+
+//コメントフォームの内容をカスタマイズ
+add_filter( 'comment_form_defaults', 'museum_comment_form_default' );
+function museum_comment_form_default( $args ) {
   unset($args['fields']['email']);
   unset($args['fields']['url']);
   $args['fields']['author'] = '<p class="comment-form-author"><label for="author">名前</label> ' .
                                 '<input id="author" name="author" type="text" value="名無しさん" size="30" maxlength="30">' .
                               '</p>';
   $args['submit_button'] = '<button name="%1$s" type="submit" id="%2$s" class="%3$s" value="%4$s">%4$s</button>';
-
   return $args;
-} );
-
+}
 
 
 //コメントフォームのコメントフィールドを一番下に移動
-add_filter( 'comment_form_fields', function($fields) {
+add_filter( 'comment_form_fields', 'move_comment_field_bottom' );
+function move_comment_field_bottom( $fields ) {
   $comment_field = $fields['comment'];
   unset( $fields['comment'] );
   $fields['comment'] = $comment_field;
   return $fields;
-} );
+}
 
 
-
-//コメントのオートリンク機能を無効化
-remove_filter( 'comment_text', 'make_clickable', 9 );
-
-
-
-//コメントで使用できるタグや属性を無効化
-(function() {
-  $invalidate_comment_tags = function($comment) {
-    if ( get_comment_type() == 'comment' ) {
-      $comment = htmlspecialchars( $comment, ENT_QUOTES );
-    }
-    return $comment;
-  };
-
-  add_filter( 'comment_text', $invalidate_comment_tags, 9 );
-  add_filter( 'comment_text_rss', $invalidate_comment_tags, 9 );
-  add_filter( 'comment_excerpt', $invalidate_comment_tags, 9 );
-})();
+//コメントでタグの使用を無効にする
+add_filter( 'comment_text', 'invalidate_comment_tags', 9 );
+add_filter( 'comment_text_rss', 'invalidate_comment_tags', 9 );
+add_filter( 'comment_excerpt', 'invalidate_comment_tags', 9 );
+function invalidate_comment_tags( $comment ) {
+  if ( get_comment_type() == 'comment' ) {
+    $comment = htmlspecialchars( $comment, ENT_QUOTES );
+  }
+  return $comment;
+}
 
 
-
-//名前が未入力の場合、「名無しさん」と表示
-add_filter( 'get_comment_author', function( $author, $comment_ID, $comment ) {
-  if ($author == __('Anonymous')) $author = '名無しさん';
+//名前フィールドの初期値に「名無しさん」を設定
+add_filter( 'get_comment_author', 'name_field_initial_value' );
+function name_field_initial_value( $author ) {
+  if ( $author == __('Anonymous') ) $author = '名無しさん';
   return $author;
-}, 10, 3 );
-
+}
 
 
 //コメントリストをカスタマイズ
