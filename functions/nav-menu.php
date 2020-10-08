2a638a0b898d8ca0242b7005bee61a8a8bf6e687
fkazu <kazukifuji@users.noreply.github.com>
2020-10-08 05:04:56 +0000

    Remove anonymous function from nav-menu.php
    nav-menu.phpから無名関数を削除。

--- functions/nav-menu.php
+++ functions/nav-menu.php
@@ -1,11 +1,14 @@
 <?php
-//ナビゲーションメニューの設定
-add_action( 'after_setup_theme', function() {
-  register_nav_menu('sidebar', 'サイドバー');
-} );
+//カスタムナビゲーションメニューを登録
+add_action( 'after_setup_theme', 'museum_nav_menu' );
+function museum_nav_menu() {
+  register_nav_menu( 'sidebar', 'サイドバー' );
+}
 
-//li要素のidを削除
-add_filter( 'nav_menu_item_id', function($id) {
+
+//メニューのアイテム要素のidを削除
+add_filter( 'nav_menu_item_id', 'remove_nav_menu_item_id' );
+function remove_nav_menu_item_id( $id ) {
   $id = null;
   return $id;
-} );
+}
