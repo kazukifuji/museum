de4e30ea0dfffd56d559c05c79b5eee5be39d9d7
fkazu <kazukifuji@users.noreply.github.com>
2020-11-06 12:07:02 +0000

    Rename module name and file name from 'setHeroHeaderHeight' to 'heroHeader'
    モジュール名とファイル名を'setHeroHeaderHeight'から'heroHeaderにリネーム。

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,14 @@ import objectFitPolyfill from 'objectFitPolyfill';
 import whatInput from 'what-input';
 
 import hamburgerButton from './features/hamburgerButton';
-import setHeroHeaderHeight from './features/setHeroHeaderHeight';
+import heroHeader from './features/heroHeader';
 import postList from './features/postList';
 
 window.addEventListener( 'load', () => {
   //ハンバーガーボタンにサイドバー開閉機能を設定
   hamburgerButton();
   //ヒーローヘッダーの高さを設定
-  setHeroHeaderHeight();
+  heroHeader();
   //投稿リストの機能を設定
   postList();
 }, { once: true } );
