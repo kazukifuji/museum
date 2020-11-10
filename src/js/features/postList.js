c6927c96b754414bfa134023ab11c10c6383efd9
fkazu <ketuago.kazu@outlook.jp>
2020-11-10 21:31:02 +0900

    Fix a bug that an error occurs when the pagination element of the post list does not exist
    投稿リストのページネーション要素が存在しない場合、エラーが発生するバグを修正。

--- src/js/features/postList.js
+++ src/js/features/postList.js
@@ -95,7 +95,7 @@ export default {
       this.status.element.style.display = 'block';
       document.querySelector( this.status.selector + ' > .infinite-scroll-request' ).style.display = 'none';
       document.querySelector( this.status.selector + ' > .infinite-scroll-error' ).style.display = 'none';
-      this.navigation.element.style.display = 'none';
+      if ( this.navigation.element ) this.navigation.element.style.display = 'none';
     }
   }, 
 }
